c1978e2596cd586b7ef19481d58dbfe789b9b39c
Xavier <xavier982@hotmail.com>
2017-09-01 15:19:33 +0200

    Front page HTML generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She is invited to enter a to-do item
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assetrEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "Buy peacock feathers" into a text box
 		inputbox.send_keys('Buy peacock feathers')
@@ -32,7 +32,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 		# There is a text box inviting her to add another item. She adds another item
 		self.fail('Finish the test!')
 
