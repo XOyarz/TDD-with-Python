a5bb45b4e904304de9a254bf8c13219bb77d77f2
Xavier <xavier982@hotmail.com>
2017-09-01 15:10:36 +0200

    All

--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 from lists.views import home_page
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
-	
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -14,6 +14,5 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')		# turns template into string
+		self.assertEqual(response.content.decode(), expected_html)		# checks that the right template (as string) is returned
