a241080a3671995a9482c76732fcb65f455c7d1c
XOyarz <xavier982@hotmail.com>
2017-09-04 16:06:18 +0200

    Update views.py

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
 
 # Create your views here.
 
-@csrf_exempt		# Added csrf_exempt because unit test "test_home_page_returns_correct_html" fails with the csrf token
 def home_page(request):
-	return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),})
+
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),})
