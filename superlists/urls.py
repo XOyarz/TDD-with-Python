dbe5317ce60d4ea077ebd718d648141af9926abd
Xavier <xavier982@hotmail.com>
2017-09-05 11:43:46 +0200

    Adding unique URLs. Using f-strings, new python 3.6 feature. REQUIRES PYTHON 3.6 TO RUN

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
 	url(r'^$', views.home_page, name='home'),
 	url(r'^lists/new$', views.new_list, name='new_list'),
-	url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-
+	# (.+) is a capture group. It passes everything between / and /
+	# as an argument to the view
+	url(r'^lists/(.+)/$', views.view_list, name='view_list'),
 ]
 
